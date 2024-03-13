abd4df180053b39871403f0bca44af25d7039ee0
Lars Strömberg <lars@dsve.se>
2024-03-13 11:30:37 +0000

    Add BookDTO.java

---
+++ src/main/java/se/dsve/bookapi/dto/BookDTO.java
@@ -0,0 +1,53 @@
+package se.dsve.bookapi.dto;
+
+import se.dsve.bookapi.model.Book;
+
+public class BookDTO {
+    private String id;
+    private String title;
+    private String author;
+    private String genre;
+
+    public BookDTO() {
+    }
+
+    public BookDTO(Book book) {
+        this.id = book.getId();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        this.genre = book.getGenre();
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
