9adbceb51fbfc772cf768dc20a8c638e48790df6
Lars Strömberg <lars@dsve.se>
2024-03-13 11:30:38 +0000

    Add Book.java

---
+++ src/main/java/se/dsve/bookapi/model/Book.java
@@ -0,0 +1,44 @@
+package se.dsve.bookapi.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document // This tells Spring to make a collection out of this class
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private String genre;
+
+    // Standard getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
