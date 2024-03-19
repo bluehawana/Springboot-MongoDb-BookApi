65fb9c8e4eff1e0e4ea74abad14b6c11a163feb6
Hongzhi Li <bluehawana@gmail.com>
2024-03-19 16:42:53 +0100

    new constructor

--- src/main/java/se/dsve/bookapi/model/Book.java
+++ src/main/java/se/dsve/bookapi/model/Book.java
@@ -16,6 +16,12 @@ public class Book {
         this.genre = genre;
     }
 
+    public Book(String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
 
     // Standard getters and setters
     public String getId() {
